5b84f2d47f0cf8fc536c7b16af5cca39c198db9e
Mickdevv <michael@devereux.fm>
2024-05-08 13:25:43 +0200

    Halfway through fetching data section

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,10 +1,22 @@
-import React from 'react'
-import products from '../products'
+import React, { useState, useEffect } from 'react'
 import Product from '../components/Product'
+import axios from 'axios'
 
 import { Row, Col } from 'react-bootstrap'
 
 function HomeScreen() {
+
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    async function fetchProducts () {
+      const { data } = await axios.get('/api/products')
+      setProducts(data)
+      console.log(data)
+    }
+    fetchProducts()
+  }, [])
+
   return (
     <div>
       <h1>Latest products</h1>
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,13 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link, useParams } from 'react-router-dom'
 import { Row, Col, Image, ListGroup, Button, Card } from 'react-bootstrap'
 import Rating from '../components/Rating'
 import products from '../products'
+import axios from 'axios'
 
 function ProductScreen() {
 
     let id = useParams().id
-    const product = products.find((p) => p._id === id)
+
+    const [product, setProduct] = useState([])
+
+    useEffect(() => {
+      async function fetchProducts () {
+        const { data } = await axios.get('/api/products/' + id)
+        setProduct(data)
+        console.log(data)
+      }
+      fetchProducts()
+    }, [])
 
   return (
     <div>
@@ -26,12 +37,8 @@ function ProductScreen() {
                     <Rating value={product.rating} color={'#f8e825'} text={`${product.numReviews} reviews`} />
                 </ListGroup.Item>
 
-                {/* <ListGroup.Item>
-                    Price : ${product.price}
-                </ListGroup.Item> */}
-
                 <ListGroup.Item>
-                    Descriiption : ${product.description}
+                    Description : ${product.description}
                 </ListGroup.Item>
             </ListGroup>
         </Col>
